9ea40213ba5ac160674c0f4219cddca9689b307c
pirabanjan <pirabanjan@gmail.com>
2022-12-10 17:37:16 -0500

    test

--- FrontEnd/Homepage.py
+++ FrontEnd/Homepage.py
@@ -1,20 +1,95 @@
+import base64
+import time
+# import hydralit_components as hc
 import streamlit as st
-
-st.set_page_config(
-    page_title = "OneTube",
-    # page_icon = ""
-)
-
-st.title("Welcome to OneTube")
-st.sidebar.success("Select a page above")
+import requests
+import subprocess
+import sys
+import json
+import numpy as np
+import requests
 
 
-if "url" not in st.session_state:
-    st.session_state['url'] = ""
+# import hydralit_components as hc
+# import time
 
-url = st.text_input("Input a URL here", st.session_state["url"])
-submit = st.button("submit")
-if submit:
-    st.session_state["url"] = url
-    st.write("your url is: ", url)
+# API request.
+def fetch(session, url):
+    try:
+        result = session.get(url)
+        return result.json()
+    except Exception:
+        return {}
 
+
+# Background Gif of the application
+def bg():
+    # side_bg_ext = 'gif'
+    file_ = open(r"C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\gif\Left-Right-Brain-Signals.webp", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+        f"""
+      <style>
+
+       [class="stApp css-ffhzg2 eczokvf1"] {{
+            background: url(data:image/gif;base64,{data_url});
+            background-size: cover;
+      }}
+      </style>
+      """,
+        unsafe_allow_html=True,
+    )
+
+
+# background audio for the home page.
+def autoplay_audio():
+    with open(r"C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\Sound\test.mp3", "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay class="stAudio" loop="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+def main():
+    st.set_page_config(
+        page_title="Cyber-safety",
+        page_icon="🤖"
+    )
+    bg()
+    autoplay_audio()
+    st.title("Welcome to Cyber-safety")
+    st.sidebar.success("Select a page above")
+    session = requests.Session()
+    with st.form("my_form"):
+        index = st.number_input("ID", min_value=0, max_value=100, key="index")
+        # index = st.text_input("Input a text")
+
+        submitted = st.form_submit_button("Submit")
+
+        # if submitted:
+        #     my_bar = st.progress(0)
+        #     for percent_complete in range(10):
+        #         time.sleep(0.1)
+        #         my_bar.progress(percent_complete + 1)
+        #     # st.write("Result")
+        #     url = 'https://z0ml9n8tn8.execute-api.us-east-1.amazonaws.com'
+        #     myobj = {'text': 'somevalue'}
+        #     x = requests.post(url, data=myobj)
+        #     print(x)
+        #     if x:
+        #         st.text(x.text)
+        #     else:
+        #         st.error("Error")
+
+
+if __name__ == '__main__':
+    main()
--- FrontEnd/Pages/2_About_Us.py
+++ FrontEnd/Pages/2_About_Us.py
@@ -1,11 +1,73 @@
+import base64
 import streamlit as st
+from PIL import Image
 
-st.title("About US")
-st.text("GitHub")
-st.text("Group Members")
 
-st.text("Apinya: LinkedIn")
-st.text('Pirabanjan: LinkedIn')
-st.text("Sarah: LinkedIn")
-st.text("Dharshana: LinkedIn")
-st.text('Kiranvir: LinkedIn')
+def bg():
+    # side_bg_ext = 'gif'
+    file_ = open(r"C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\gif\giphy (1).gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+        f"""
+      <style>
+
+       [class="stApp css-ffhzg2 eczokvf1"] {{
+            background: url(data:image/gif;base64,{data_url});
+            background-size: cover;
+      }}
+      </style>
+      """,
+        unsafe_allow_html=True,
+    )
+
+
+# background audio for the home page.
+def autoplay_audio():
+    with open(r"C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\Sound\motivational-day-112790.mp3", "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio autoplay class="stAudio" loop="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
+
+
+def main():
+    bg()
+    autoplay_audio()
+    st.title("About US")
+    st.text("GitHub")
+
+    st.header("Group Members")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    praba = Image.open(r'C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\Images\Pirabanjan.jpg')
+    api = Image.open(r'C:\Users\DELL\PycharmProjects\pythonProject\FrontEnd\Images\api.jpeg')
+
+    with col1:
+        st.image(api)
+        st.write('Apinya: [LinkedIn](https://www.linkedin.com/in/apinya-t/)')
+    with col2:
+        st.image(praba)
+        st.write('Pirabanjan: [LinkedIn](https://www.linkedin.com/in/pirabanjan-kirupaharan/)')
+    with col3:
+        st.text("Sarah: LinkedIn")
+        st.write('Sarah: [LinkedIn](https://www.linkedin.com/in/pirabanjan-kirupaharan/)')
+    with col4:
+        st.text("Dharshana: LinkedIn")
+        st.write('Dharshana: [LinkedIn](https://www.linkedin.com/in/pirabanjan-kirupaharan/)')
+    with col5:
+        st.text('Kiranvir: LinkedIn')
+        st.write('Dharshana: [LinkedIn](https://www.linkedin.com/in/pirabanjan-kirupaharan/)')
+
+
+if __name__ == '__main__':
+    main()
